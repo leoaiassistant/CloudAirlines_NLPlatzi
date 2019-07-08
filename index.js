2b98250f41431fbe9e8053f8ea8734d316f1e7ac
GDG CR <leo@gdgpuravida.xyz>
2019-07-07 20:00:36 -0600

    update.firestore

--- index.js
+++ index.js
@@ -3,24 +3,23 @@
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
-const video = 'https://www.youtube.com/watch?v=U9hq83ryFj0';
-const wedo = 'https://media.giphy.com/media/t8LaaaPNOYVJfImLNu/giphy.gif';
-const buceo = 'https://media.giphy.com/media/3oEhmGYdZDCsGANSUw/giphy.gif';
+const videowelcome = 'https://www.youtube.com/watch?v=U9hq83ryFj0';
 const welcome = 'https://media3.giphy.com/media/9Y5dai0r8F9xb5FCrw/giphy.gif?cid=3640f6095c96e7c87a56424a41e5d14f';
 const welcome2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCI7tShlyVeubGtVbh6XTCPNfZVlXFFYOEO55zskrSjLzwEKUQ';
-const infomk = 'https://media3.giphy.com/media/atZII8NmbPGw0/giphy.gif?cid=790b76115ce5b0944366663159488200&rid=giphy.gif';
+const infovuelos = 'https://media3.giphy.com/media/atZII8NmbPGw0/giphy.gif?cid=790b76115ce5b0944366663159488200&rid=giphy.gif';
 
 const admin = require('firebase-admin');
 var firebaseConfig = {
-  apiKey: "Ingrese su ID",
-    authDomain: "Ingrese su ID",
-    databaseURL: "Ingrese su ID",
-    projectId: "Ingrese su ID",
-    storageBucket: "Ingrese su ID",
-    messagingSenderId: "Ingrese su ID"
+  authDomain: "tedplatzi-9e732.firebaseapp.com",
+    databaseURL: "https://tedplatzi-9e732.firebaseio.com",
+    projectId: "tedplatzi-9e732",
+    storageBucket: "tedplatzi-9e732.appspot.com",
+    messagingSenderId: "962756455782"
   };
-admin.initializeApp(firebaseConfig);
-var database = admin.database();
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
@@ -29,64 +28,54 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
  
 function welcome(agent) {
-    agent.add(`Bienvenid@ te saluda Ted 🤖, la asistente virtual para reservar vuelos.`);
+    agent.add(`Bienvenid@ te saluda Ted 🤖, la asistente virtual de Cloud Airliness`);
     agent.add(new Card({
-        title: `Platzi NLP `,
+        title: `NLP Demo `,
         imageUrl: welcome2,
-        text: `Muy pronto disponible para Google Assistant & Web`,
+        text: `Automatización de reservación de vuelos en una base datos en tiempo real en la nube.`,
         buttonText: 'Ver Video', 
-        buttonUrl: video
+        buttonUrl: videowelcome
       })
     );
-    agent.add(`Selecciona el tema que más te interesa y en el que fui entrenado:`);
- agent.add(new Suggestion(`Ver Vuelos ✈️`));   
- agent.add(new Suggestion(`Comprar Ticket  💳`));
-	agent.add(new Suggestion(`Precios`));
-    agent.add(new Suggestion(`Reservaciones`));
+    agent.add(`Recuerda que fui entrenada:`);
+ agent.add(new Suggestion(`Reservar vuelos`));   
+ agent.add(new Suggestion(`Códigos aeropuertos`));
+	
    
   }
 function fallback(agent) {
     agent.add(`Recuerda que fui entrenada en:`);
-  agent.add(new Suggestion(`Reservaciones vuelos ✈️`));   
- agent.add(new Suggestion(`Mis reservaciones`));
+ agent.add(new Suggestion(`Reservar vuelos`));   
+ agent.add(new Suggestion(`Códigos aeropuertos`));
   
   }
   
 function info(agent) { 
- agent.add(`Excelente decisión, en Pura Vida Airliness le podemos asesorar con:`);
- agent.add(new Suggestion(`Comprar Vuelos 🚀`));   
- agent.add(new Suggestion(`Reservar Vuelos 💻`));
+ agent.add(`Para conocer el código internacional IATA del aeropuerto, indique su ciudad y país:`);
+ agent.add(new Suggestion(`Reservar vuelo`));   
+
 
 }
 
-function mk(agent) {
-    agent.add(`En los cursos de mercadeo digital de Platzi podrás transformar tu carreras:`);
-    agent.add(new Card({
-        title: `Marketing Demo `,
-        imageUrl: infomk,
-        text: `Aprenderás ténicas de análisis avanzado de datos y más`,
-        buttonText: 'Ver Video', 
-        buttonUrl: video
-      })
-    );
-    agent.add(`Ya te decidiste?`);
- agent.add(new Suggestion(`Matricular curso 💻`));
-	agent.add(new Suggestion(`Pagar 💳`));
-   
-  }
 
-function matricula(agent) {
-let name = agent.parameters.name;
-let cursosplatzi = agent.parameters.cursosplatzi;
-    return database.ref('users')
-       .push({
-            phoneNumber: agent.parameters.phoneNumber,
-            email: agent.parameters.email,
-            cursosplatzi: agent.parameters.cursosplatzi,
-			name: agent.parameters.name,
-          })
-      .then(()=> {
-	    agent.add(`Excelente ${name} su reservación ${cursosplatzi} fue realizada con éxito ✅`);
+function book (agent) {
+    // Get parameter from Dialogflow with the string to add to the database
+    const name = agent.parameters.name;
+	const email = agent.parameters.email;
+	const cursoplatzi = agent.parameters.cursoplatzi;
+
+    // Get the database collection 'dialogflow' and document 'agent' and store
+    // the document  {entry: "<value of database entry>"} in the 'agent' document
+    const dialogflowAgentRef = db.collection('matriculados').doc();
+    return db.runTransaction(t => {
+      t.set(dialogflowAgentRef, {Nombre: name});
+
+      return Promise.resolve('Write complete');
+    }).then(doc => {
+      agent.add(`Excelente "${name}" su curso fue matriculado con éxito.`);
+    }).catch(err => {
+      console.log(`Error writing to Firestore: ${err}`);
+      agent.add(`Failed to write "${name}" to the Firestore database.`);
     });
   }
   
@@ -96,8 +85,7 @@ let cursosplatzi = agent.parameters.cursosplatzi;
 intentMap.set('Default Welcome Intent', welcome);
 intentMap.set('Default Fallback Intent', fallback);
 intentMap.set('info.cursos', info);
-intentMap.set('info.cursos.mk', mk);
-intentMap.set('matricular.curso', matricula);
+intentMap.set('book', book);
   
   agent.handleRequest(intentMap);
 });
